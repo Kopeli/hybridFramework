c39e2c223e2bdce32a817c179b9c15a1945422ab
Kopeli <Kopeli@Muhammet>
2019-01-18 19:55:37 -0500

    second commit

--- src/test/java/common/newPatient.java
+++ src/test/java/common/newPatient.java
@@ -7,12 +7,16 @@ public class newPatient {
 
 	WebDriver driver;
 	
+	
+	
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kopeli\\Documents\\mahbub\\chromedriver");
 		driver = new ChromeDriver();
 		driver.get("http://demo.openemr.io/openemr/");
 	}
-	
+	public void mTT() {
+		
+	}
 	public void closeBrowser() {
 		driver.close();
 	}
